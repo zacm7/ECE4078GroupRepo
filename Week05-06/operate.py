0423ec3f6835f490638608915dcd2be9a141215e
spur0010 <166361166+spur0010@users.noreply.github.com>
2025-08-28 15:53:13 +1000

    M2

--- Week05-06/operate.py
+++ Week05-06/operate.py
@@ -1,27 +1,25 @@
-# teleoperate the robot, perform SLAM and object detection
+# teleoperate the robot and perform SLAM
+# will be extended in following milestones for system integration
 
-import os
-import sys
-import time
-import cv2
+# basic python packages
 import numpy as np
+import cv2 
+import os, sys
+import time
 
 # import utility functions
-sys.path.insert(0, "{}/util".format(os.getcwd()))
-from util.pibot import PenguinPi    # access the robot
-import util.DatasetHandler as dh    # save/load functions
-import util.measure as measure      # measurements
-import pygame                       # python package for GUI
-import shutil                       # python package for file operations
+#sys.path.insert(0, "{}/util".format(os.getcwd()))
+from util.pibot import PenguinPi # access the robot
+import util.DatasetHandler as dh # save/load functions
+import util.measure as measure # measurements
+import pygame # python package for GUI
+import shutil # python package for file operations
 
-# import SLAM components you developed in M2
+# import SLAM components
 sys.path.insert(0, "{}/slam".format(os.getcwd()))
 from slam.ekf import EKF
 from slam.robot import Robot
 import slam.aruco_detector as aruco
-
-# import YOLO components 
-from YOLO.detector import Detector
 
 
 class Operate:
@@ -32,7 +30,7 @@ class Operate:
         else:
             shutil.rmtree(self.folder)
             os.makedirs(self.folder)
-
+        
         # initialise data parameters
         if args.play_data:
             self.pibot = dh.DatasetPlayer("record")
@@ -42,14 +40,14 @@ class Operate:
         # initialise SLAM parameters
         self.ekf = self.init_ekf(args.calib_dir, args.ip)
         self.aruco_det = aruco.aruco_detector(
-            self.ekf.robot, marker_length=0.07)  # size of the ARUCO markers
+            self.ekf.robot, marker_length = 0.07) # size of the ARUCO markers
 
         if args.save_data:
             self.data = dh.DatasetWriter('record')
         else:
             self.data = None
         self.output = dh.OutputWriter('lab_output')
-        self.command = {'motion': [0, 0],
+        self.command = {'motion':[0, 0], 
                         'inference': False,
                         'output': False,
                         'save_inference': False,
@@ -62,31 +60,23 @@ class Operate:
         self.double_reset_comfirm = 0
         self.image_id = 0
         self.notification = 'Press ENTER to start SLAM'
-        self.pred_notifier = False
         # a 5min timer
         self.count_down = 300
         self.start_time = time.time()
         self.control_clock = time.time()
         # initialise images
-        self.img = np.zeros([240, 320, 3], dtype=np.uint8)
-        self.aruco_img = np.zeros([240, 320, 3], dtype=np.uint8)
-        self.detector_output = np.zeros([240, 320], dtype=np.uint8)
-        if args.yolo_model == "":
-            self.detector = None
-            self.yolo_vis = cv2.imread('pics/8bit/detector_splash.png')
-        else:
-            self.detector = Detector(args.yolo_model)
-            self.yolo_vis = np.ones((240, 320, 3)) * 100
+        self.img = np.zeros([240,320,3], dtype=np.uint8)
+        self.aruco_img = np.zeros([240,320,3], dtype=np.uint8)
         self.bg = pygame.image.load('pics/gui_mask.jpg')
 
     # wheel control
-    def control(self):
+    def control(self):    
         if args.play_data:
-            lv, rv = self.pibot.set_velocity()
+            lv, rv = self.pibot.set_velocity()            
         else:
             lv, rv = self.pibot.set_velocity(
                 self.command['motion'])
-        if self.data is not None:
+        if not self.data is None:
             self.data.write_keyboard(lv, rv)
         dt = time.time() - self.control_clock
         # running in sim
@@ -97,12 +87,11 @@ class Operate:
             drive_meas = measure.Drive(lv, -rv, dt)
         self.control_clock = time.time()
         return drive_meas
-
+        
     # camera control
     def take_pic(self):
         self.img = self.pibot.get_image()
-
-        if self.data is not None:
+        if not self.data is None:
             self.data.write_image(self.img)
 
     # SLAM with ARUCO markers       
@@ -117,28 +106,12 @@ class Operate:
                 self.notification = 'Recover failed, need >2 landmarks!'
                 self.ekf_on = False
             self.request_recover_robot = False
-        elif self.ekf_on:  # and not self.debug_flag:
+        elif self.ekf_on: # and not self.debug_flag:
             self.ekf.predict(drive_meas)
             self.ekf.add_landmarks(lms)
             self.ekf.update(lms)
 
-    # using computer vision to detect targets
-    def detect_target(self):
-        if self.command['inference'] and self.detector is not None:
-            # need to convert the colour before passing to YOLO
-            yolo_input_img = cv2.cvtColor(self.img, cv2.COLOR_RGB2BGR)
-
-            self.detector_output, self.yolo_vis = self.detector.detect_single_image(yolo_input_img)
-
-            # covert the colour back for display purpose
-            self.yolo_vis = cv2.cvtColor(self.yolo_vis, cv2.COLOR_RGB2BGR)
-
-            # self.command['inference'] = False     # uncomment this if you do not want to continuously predict
-            self.file_output = (yolo_input_img, self.ekf)
-
-            # self.notification = f'{len(self.detector_output)} target type(s) detected'
-
-    # save raw images taken by the camera
+    # save images taken by the camera
     def save_image(self):
         f_ = os.path.join(self.folder, f'img_{self.image_id}.png')
         if self.command['save_image']:
@@ -159,7 +132,7 @@ class Operate:
         scale = np.loadtxt(fileS, delimiter=',')
         if ip == 'localhost':
             scale /= 2
-        fileB = "{}baseline.txt".format(datadir)
+        fileB = "{}baseline.txt".format(datadir)  
         baseline = np.loadtxt(fileB, delimiter=',')
         robot = Robot(baseline, scale, camera_matrix, dist_coeffs)
         return EKF(robot)
@@ -170,16 +143,6 @@ class Operate:
             self.output.write_map(self.ekf)
             self.notification = 'Map is saved'
             self.command['output'] = False
-        # save inference with the matching robot pose and detector labels
-        if self.command['save_inference']:
-            if self.file_output is not None:
-                # image = cv2.cvtColor(self.file_output[0], cv2.COLOR_RGB2BGR)
-                self.pred_fname = self.output.write_image(self.file_output[0],
-                                                          self.file_output[1])
-                self.notification = f'Prediction is saved to {operate.pred_fname}'
-            else:
-                self.notification = f'No prediction in buffer, save ignored'
-            self.command['save_inference'] = False
 
     # paint the GUI            
     def draw(self, canvas):
@@ -189,39 +152,33 @@ class Operate:
         h_pad = 20
 
         # paint SLAM outputs
-        ekf_view = self.ekf.draw_slam_state(res=(320, 480 + v_pad),
-                                            not_pause=self.ekf_on)
-        canvas.blit(ekf_view, (2 * h_pad + 320, v_pad))
+        ekf_view = self.ekf.draw_slam_state(res=(320, 480+v_pad),
+            not_pause = self.ekf_on)
+        canvas.blit(ekf_view, (2*h_pad+320, v_pad))
         robot_view = cv2.resize(self.aruco_img, (320, 240))
-        self.draw_pygame_window(canvas, robot_view,
+        self.draw_pygame_window(canvas, robot_view, 
                                 position=(h_pad, v_pad)
                                 )
 
-        # for target detector (M3)
-        detector_view = cv2.resize(self.yolo_vis, (320, 240), cv2.INTER_NEAREST)
-        self.draw_pygame_window(canvas, detector_view,
-                                position=(h_pad, 240 + 2 * v_pad)
-                                )
-
         # canvas.blit(self.gui_mask, (0, 0))
-        self.put_caption(canvas, caption='SLAM', position=(2 * h_pad + 320, v_pad))
-        self.put_caption(canvas, caption='Detector',
-                         position=(h_pad, 240 + 2 * v_pad))
+        self.put_caption(canvas, caption='SLAM', position=(2*h_pad+320, v_pad)) # M2
+        self.put_caption(canvas, caption='Detector (M2)',
+                         position=(h_pad, 240+2*v_pad)) # M3
         self.put_caption(canvas, caption='PiBot Cam', position=(h_pad, v_pad))
 
         notifiation = TEXT_FONT.render(self.notification,
-                                       False, text_colour)
-        canvas.blit(notifiation, (h_pad + 10, 596))
+                                          False, text_colour)
+        canvas.blit(notifiation, (h_pad+10, 596))
 
         time_remain = self.count_down - time.time() + self.start_time
         if time_remain > 0:
             time_remain = f'Count Down: {time_remain:03.0f}s'
-        elif int(time_remain) % 2 == 0:
+        elif int(time_remain)%2 == 0:
             time_remain = "Time Is Up !!!"
         else:
             time_remain = ""
         count_down_surface = TEXT_FONT.render(time_remain, False, (50, 50, 50))
-        canvas.blit(count_down_surface, (2 * h_pad + 320 + 5, 530))
+        canvas.blit(count_down_surface, (2*h_pad+320+5, 530))
         return canvas
 
     @staticmethod
@@ -230,28 +187,30 @@ class Operate:
         view = pygame.surfarray.make_surface(cv2_img)
         view = pygame.transform.flip(view, True, False)
         canvas.blit(view, position)
-
+    
     @staticmethod
     def put_caption(canvas, caption, position, text_colour=(200, 200, 200)):
         caption_surface = TITLE_FONT.render(caption,
-                                            False, text_colour)
-        canvas.blit(caption_surface, (position[0], position[1] - 25))
+                                          False, text_colour)
+        canvas.blit(caption_surface, (position[0], position[1]-25))
 
-    # keyboard teleoperation, replace with your M1 codes if preferred        
+    # keyboard teleoperation        
     def update_keyboard(self):
         for event in pygame.event.get():
+            ########### replace with your M1 codes ###########
             # drive forward
             if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-                self.command['motion'][0] = min(self.command['motion'][0] + 1, 1)
+                self.command['motion'] = [1, 0]  # drive forward: linear=10, angular=0
             # drive backward
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-                self.command['motion'][0] = max(self.command['motion'][0] - 1, -1)
+                self.command['motion'] = [-1, 0]  # drive backward: linear=-10, angular=0
             # turn left
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-                self.command['motion'][1] = min(self.command['motion'][1] + 1, 1)
+                self.command['motion'] = [0, 1]  # turn left: linear=0, angular=-10
             # drive right
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-                self.command['motion'][1] = max(self.command['motion'][1] - 1, -1)
+                self.command['motion'] = [0, -1]   # turn right: linear=0, angular=10
+            ####################################################
             # stop
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 self.command['motion'] = [0, 0]
@@ -265,7 +224,7 @@ class Operate:
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_r:
                 if self.double_reset_comfirm == 0:
                     self.notification = 'Press again to confirm CLEAR MAP'
-                    self.double_reset_comfirm += 1
+                    self.double_reset_comfirm +=1
                 elif self.double_reset_comfirm == 1:
                     self.notification = 'SLAM Map is cleared'
                     self.double_reset_comfirm = 0
@@ -289,12 +248,6 @@ class Operate:
                         self.notification = 'SLAM is running'
                     else:
                         self.notification = 'SLAM is paused'
-            # run object detector
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
-                self.command['inference'] = True
-            # save object detection outputs
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_n:
-                self.command['save_inference'] = True
             # quit
             elif event.type == pygame.QUIT:
                 self.quit = True
@@ -304,7 +257,7 @@ class Operate:
             pygame.quit()
             sys.exit()
 
-
+        
 if __name__ == "__main__":
     import argparse
 
@@ -314,23 +267,22 @@ if __name__ == "__main__":
     parser.add_argument("--calib_dir", type=str, default="calibration/param/")
     parser.add_argument("--save_data", action='store_true')
     parser.add_argument("--play_data", action='store_true')
-    parser.add_argument("--yolo_model", default='YOLO/model/yolov8_model.pt')
     args, _ = parser.parse_known_args()
-
-    pygame.font.init()
+    
+    pygame.font.init() 
     TITLE_FONT = pygame.font.Font('pics/8-BitMadness.ttf', 35)
     TEXT_FONT = pygame.font.Font('pics/8-BitMadness.ttf', 40)
-
+    
     width, height = 700, 660
     canvas = pygame.display.set_mode((width, height))
-    pygame.display.set_caption('ECE4078 2023 Lab')
+    pygame.display.set_caption('ECE4078 Lab')
     pygame.display.set_icon(pygame.image.load('pics/8bit/pibot5.png'))
     canvas.fill((0, 0, 0))
     splash = pygame.image.load('pics/loading.png')
     pibot_animate = [pygame.image.load('pics/8bit/pibot1.png'),
                      pygame.image.load('pics/8bit/pibot2.png'),
                      pygame.image.load('pics/8bit/pibot3.png'),
-                     pygame.image.load('pics/8bit/pibot4.png'),
+                    pygame.image.load('pics/8bit/pibot4.png'),
                      pygame.image.load('pics/8bit/pibot5.png')]
     pygame.display.update()
 
@@ -344,7 +296,7 @@ if __name__ == "__main__":
         canvas.blit(splash, (0, 0))
         x_ = min(counter, 600)
         if x_ < 600:
-            canvas.blit(pibot_animate[counter % 10 // 2], (x_, 565))
+            canvas.blit(pibot_animate[counter%10//2], (x_, 565))
             pygame.display.update()
             counter += 2
 
@@ -357,7 +309,10 @@ if __name__ == "__main__":
         operate.update_slam(drive_meas)
         operate.record_data()
         operate.save_image()
-        operate.detect_target()
         # visualise
         operate.draw(canvas)
         pygame.display.update()
+
+
+
+
